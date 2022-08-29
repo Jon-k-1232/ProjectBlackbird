def15c1b52071b99d78f58a1c6fa622115510f68
Jon Kimmel <Jonkimmel16@gmail.com>
2022-08-28 17:12:33 -0700

    config update

--- ProjectBlackBird_Backend/config.js
+++ ProjectBlackBird_Backend/config.js
@@ -27,8 +27,6 @@ module.exports = {
   DATABASE_URL: 'jka_test',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://@localhost/jka_test',
   API_TOKEN: process.env.API_TOKEN,
-  ACCESS_TOKEN: process.env.ACCESS_TOKEN,
-  REFRESH_TOKEN: process.env.REFRESH_TOKEN,
   JWT_EXPIRATION: '10h',
   defaultDaysInPast: 730,
   // interest Calculation
