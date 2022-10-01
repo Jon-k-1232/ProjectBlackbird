d0eacfd84694774ffae96990d53712982c67860a
Jon Kimmel <Jonkimmel16@gmail.com>
2022-10-01 15:12:38 -0700

    add overpayment alert for write offs

--- ProjectBlackBird_Frontend/src/Components/TransactionFormOptions/WriteOffOptions.js
+++ ProjectBlackBird_Frontend/src/Components/TransactionFormOptions/WriteOffOptions.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Container, Stack, TextField, Autocomplete } from '@mui/material';
+import { Container, Stack, TextField, Autocomplete, Alert } from '@mui/material';
 import InvoiceConfirmation from './InvoiceConfirmation';
 
 export default function WriteOffOptions({
@@ -13,6 +13,34 @@ export default function WriteOffOptions({
 }) {
   const [selectedCyclePeriod, setSelectedCyclePeriod] = useState('');
   const [selectedCycleInputValue, setSelectedCycleInputValue] = useState('');
+  const [foundInvoice, setFoundInvoice] = useState({});
+  const [overPaymentAlert, setOverPaymentAlert] = useState(false);
+
+  /**
+   * Sets user input for amount to pay
+   * @param {*} e
+   */
+  const setTransactionAmount = e => {
+    e.target.value >= 0 && setSelectedAmount(e.target.value);
+    setDisableSubmit(false);
+    setTotalTransaction(-Math.abs(e.target.value * selectedQuantity).toFixed(2));
+
+    if (e.target.value > foundInvoice.unPaidBalance) {
+      setOverPaymentAlert(true);
+    } else {
+      setOverPaymentAlert(false);
+    }
+  };
+
+  /**
+   * Sets invoice number and finds a matching invoice
+   * @param {*} invoiceNumber
+   */
+  const setAndSaveInvoices = invoiceNumber => {
+    setInvoice(invoiceNumber);
+    const foundInputInvoice = outstandingInvoices.rawData.find(invoiceRec => invoiceRec.invoiceNumber === invoiceNumber);
+    setFoundInvoice(foundInputInvoice);
+  };
 
   return (
     <Container>
@@ -33,7 +61,7 @@ export default function WriteOffOptions({
           <InvoiceConfirmation
             outstandingInvoices={outstandingInvoices}
             setDisableSubmit={boolValue => setDisableSubmit(boolValue)}
-            setInvoice={invoiceNumber => setInvoice(invoiceNumber)}
+            setInvoice={invoiceNumber => setAndSaveInvoices(invoiceNumber)}
           />
         )}
         <Stack direction={{ xs: 'column', sm: 'row' }} spacing={{ xs: 1, sm: 2, md: 8 }}>
@@ -43,14 +71,11 @@ export default function WriteOffOptions({
             max='10'
             label='Write Off Amount'
             value={selectedAmount}
-            onChange={e => {
-              e.target.value >= 0 && setSelectedAmount(e.target.value);
-              setDisableSubmit(false);
-              setTotalTransaction(-Math.abs(e.target.value * selectedQuantity).toFixed(2));
-            }}
+            onChange={e => setTransactionAmount(e)}
             helperText='* Amount will reflect as negative in total amount. This credits the account/job.'
           />
         </Stack>
+        <Stack>{overPaymentAlert && <Alert severity='error'>Input Amount Exceeds Invoice Balance</Alert>}</Stack>
       </Stack>
     </Container>
   );
